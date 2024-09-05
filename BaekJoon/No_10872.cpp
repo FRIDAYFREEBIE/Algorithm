635c80fe0320097bb1ebf373fb68e8fe57135a74
FRIDAYFREEBIE <sheepyubin06@gmail.com>
2024-09-05 13:57:07 +0900

    2024-09-04 업로드

--- BaekJoon/No_10872.cpp
+++ BaekJoon/No_10872.cpp
@@ -1,14 +1,27 @@
-#include<stdio.h>
+#include <iostream>
 
-main()
-{
-    int n, x, t;
-    
-    scanf("%d%d", &n, &x);
-    
-    for(int i=0; i<n; i++){
-        scanf("%d", &t);
-        if(t<x)
-            printf("%d ", t);
-    }
+using namespace std;
+
+int main(){
+  ios :: sync_with_stdio(false);
+  cin.tie(0);
+
+  int  n; 
+
+  cin >> n;
+
+  if(n == 0){
+    cout << 1 << "\n";
+    return 0;
+  }
+
+  int result = 1;
+
+  for(int i = 1; i <= n; i++){
+    result *= i;
+  }
+  
+  cout << result << "\n";
+
+  return 0;
 }
