dd1d13c686c562dcc834b341294a0eda805cfbb6
FRIDAYFREEBIE <sheepyubin06@gmail.com>
2024-09-07 15:12:31 +0900

    2024-09-06 업로드

---
+++ BaekJoon/No_2775.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  ios ::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  int t, k, n;
+
+  cin >> t;
+
+  for(int i = 0; i < t; i++){
+    cin >> k >> n;
+  }
+
+  return 0;
+}
