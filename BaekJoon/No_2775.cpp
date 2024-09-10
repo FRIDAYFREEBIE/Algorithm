92cb8bd760c3180011915e7aef2e1f8d990dd2b9
FRIDAYFREEBIE <sheepyubin06@gmail.com>
2024-09-10 11:03:42 +0900

    2024-09-10 업로드

--- BaekJoon/No_2775.cpp
+++ BaekJoon/No_2775.cpp
@@ -1,19 +1,51 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
+
+int GetPeople(int k, int n);
 
 int main() {
   ios ::sync_with_stdio(false);
   cin.tie(NULL);
 
-  int t, k, n;
+  int t, k, n, sum = 0;
 
   cin >> t;
 
   for(int i = 0; i < t; i++){
     cin >> k >> n;
+
+    cout << GetPeople(k, n) << "\n";
   }
 
   return 0;
 }
+
+// 재귀함수
+int GetPeople(int k, int n){
+  if(k == 0) return n;
+  if(n == 1) return 1;
+
+  return(GetPeople(k - 1, n) + GetPeople(k, n - 1));
+}
+
+// 2-3
+
+// 2-1 2-2 2-3
+// 1-1 1-2 1-3
+// 0-1 0-2 0-3
+
+// 2-3 = 2-2 + 1-3
+
+// 2-2
+// = 2-1 + 1-2
+// = 1 + 1-1 + 0-2
+// = 1 + 1 + 2
+// = 4
+
+
+// 1-3
+// = 1-2 + 0-3
+// = 1-1 + 0-2 + 3
+// = 1 + 2 + 3
+// = 6
---
+++ BaekJoon/No_29766.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int GetPeople(int k, int n);
+
+int main() {
+  ios ::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  string s;
+
+  cin >> s;
+
+  return 0;
+}
