89b3df3ea954f6645fdd4046aa8fa158cc9c64a2
FRIDAYFREEBIE <sheepyubin06@gmail.com>
2024-09-26 22:00:39 +0900

    2024-09-26

--- BaekJoon/No_10989.cpp
+++ BaekJoon/No_10989.cpp
@@ -6,7 +6,28 @@ int main() {
   ios ::sync_with_stdio(false);
   cin.tie(NULL);
 
-  
+  int n;
+
+  cin >> n;
+
+  int max = 0;
+  int arr[10001] = {0};
+
+  for(int i = 0; i < n; i++){
+    int temp;
+
+    cin >> temp;
+
+    if(temp > max) max = temp;
+
+    arr[temp - 1] ++;
+  }
+
+  for(int i = 0; i < max; i++){
+    for(int j = 0; j < arr[i]; j++){
+      cout << i + 1<< "\n";
+    }
+  }
 
   return 0;
 }
