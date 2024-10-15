9ddaac3ed750852088681270e06149e5c1506b08
FRIDAYFREEBIE <sheepyubin06@gmail.com>
2024-10-15 19:00:48 +0900

    2024-20-15

--- BaekJoon/No_4949.cpp
+++ BaekJoon/No_4949.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <stack>
 
 using namespace std;
 
@@ -6,7 +8,51 @@ int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
 
-  
+  string str = " ";
+
+  while (true) {
+    getline(cin, str);
+    
+    if (str == ".") break;
+
+    stack<char> s;
+    bool isBalanced = true;
+
+    for (int i = 0; i < str.size(); i++) {
+      char ch = str[i];
+
+      if (ch == '(' || ch == '[') {
+        s.push(ch);
+      }
+      else if (ch == ')') {
+        if (s.empty() || s.top() != '(') {
+          isBalanced = false;
+          break;
+        }
+
+        s.pop();
+      }
+      else if (ch == ']') {
+        if (s.empty() || s.top() != '[') {
+          isBalanced = false;
+          break;
+        }
+
+        s.pop();
+      }
+    }
+
+    if (!s.empty()) {
+      isBalanced = false;
+    }
+
+    if (isBalanced) {
+      cout << "yes" << "\n";
+    }
+    else {
+      cout << "no" << "\n";
+    }
+  }
 
   return 0;
 }
