af7c83f79542ff8986f3b77519d9b2745b8cfd1b
FRIDAYFREEBIE <sheepyubin06@gmail.com>
2024-12-16 09:04:56 +0900

    2024-12-16

--- BaekJoon/No_1449.cpp
+++ BaekJoon/No_1449.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,7 +8,24 @@ int main() {
   cin.tie(NULL);
   cout.tie(NULL);
 
-  
+  int n, l;
+  cin >> n >> l;
+
+  int location[1000], start, tape =  0;
+  for(int i = 0; i < n; i++) cin >> location[i];
+
+  sort(location, location + n);
+
+  start =  location[0];
+
+  for(int i  = 1; i < n; i++){
+    if(l <= location[i] - start){
+      tape++;
+      start =  location[i];
+    }
+  }
+
+  cout << tape + 1 << "\n";
 
   return 0;
 }
