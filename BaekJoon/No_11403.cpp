08c081eb5fa06bbc7b00d4f307c88f1b05e3fc9c
FRIDAYFREEBIE <sheepyubin06@gmail.com>
2025-11-30 22:45:57 +0900

    2025-11-30

--- BaekJoon/No_11403.cpp
+++ BaekJoon/No_11403.cpp
@@ -2,23 +2,51 @@
 
 using namespace std;
 
-int adj[101][101];
+int graph[101][101];
+int result[101][101];
+int n;
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
   cout.tie(0);
 
-  int n;
   cin >> n;
+
+  // 입력
+  for(int i = 0; i < n; i++)
+    for(int j = 0; j < n; j++)
+      cin >> graph[i][j];
+
+  // 복사
+  for(int i = 0; i < n; i++)
+    for(int j = 0; j < n; j++)
+      result[i][j] = graph[i][j];
+
+  // 플로이드 워셜
+  for(int k = 0; k < n; k++)
+    for(int i = 0; i < n; i++)
+      for(int j = 0; j < n; j++)
+        if(result[i][k] == 1 && result[k][j] == 1)
+          result[i][j] = 1;
+
+  cout << "\n";
 
   for(int i = 0; i < n; i++){
     for(int j = 0; j < n; j++){
-      cin >> adj[i][j];
+      cout << result[i][j] << " ";
     }
+    cout << "\n";
   }
-
   
+  // // Debug
+  // cout << "\n";
+  // for(int i = 0; i < n; i++){
+  //   for(int j = 0; j < n; j++){
+  //     cout << graph[i][j] << " ";
+  //   }
+  //   cout << "\n";
+  // }
 
   return 0;
 }
